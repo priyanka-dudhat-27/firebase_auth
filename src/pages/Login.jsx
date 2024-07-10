9b465cbaea29a7cd30340f72f1d85c35f68296df
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-07-10 15:04:49 +0530

    google auth done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,13 @@
 // src/pages/Login.jsx
 import React, { useEffect, useState } from 'react';
 import { auth, provider } from '../firebaseConfig'; // Adjust the path to import from firebaseConfig.js
-import { signInWithPopup ,signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth';
 import Home from './Home'; // Ensure the path is correct
 
 const Login = () => {
   const [value, setValue] = useState("");
-  const [email,setEmail]=useState("");
-  const [password,setPassword]=useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLoginWithGoogle = () => {
     signInWithPopup(auth, provider)
@@ -17,7 +17,22 @@ const Login = () => {
         localStorage.setItem("email", data.user.email);
       })
       .catch((error) => {
-        console.error("Error during login: ", error);
+        console.error("Error during login with Google: ", error);
+      });
+  };
+
+  const handleLoginWithEmail = () => {
+    auth.signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log('User signed in:', user);
+        setValue(user.email);
+        localStorage.setItem("email", user.email);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error('Error:', errorCode, errorMessage);
       });
   };
 
@@ -25,28 +40,11 @@ const Login = () => {
     setValue(localStorage.getItem("email"));
   }, []);
 
-  // login with email and password
-  const handleLogin=()=>{
-    auth.signInWithEmailAndPassword(email, password)
-     .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log('User signed in:', user);
-      })
-     .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error('Error:', errorCode, errorMessage);
-      });
-  }
-
-
   return (
     <div>
-
-      <input type="email" placeholder='Enter email' value={email} onChange={(e)=>setEmail(e.target.value)} />
-      <input type="password" placeholder='Enter password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
-      <button onClick={handleLogin}>Login</button>
+      <input type="email" placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLoginWithEmail}>Login</button>
 
       {value 
         ? <Home />
